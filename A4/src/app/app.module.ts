08c233ee4fc704e90994147d6d9408551525c724
unknown <taind2504@gmail.com>
2020-11-10 12:26:59 -0400

    working

--- A4/src/app/app.module.ts
+++ A4/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { PostComponent } from './post/post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostCardComponent } from './post-card/post-card.component';
+import { LatestPostsComponent } from './latest-posts/latest-posts.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    PostComponent,
+    PageNotFoundComponent,
+    PostCardComponent,
+    LatestPostsComponent,
+    CategoriesComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule,
