d2aac5d40427a148c571d95fd6e01c8aa1eae58d
unknown <taind2504@gmail.com>
2020-11-24 14:44:17 -0500

    1

--- A4/src/app/app.component.ts
+++ A4/src/app/app.component.ts
@@ -8,3 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'A4';
 }
+
+/*********************************************************************************
+* WEB422 – Assignment 04
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: DucTai Nguyen Student ID: 147942171 Date: 11/11/2020
+*
+********************************************************************************/
